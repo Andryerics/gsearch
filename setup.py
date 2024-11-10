f0f6e9421985272a21e4c6b9c7d31e2ccac37167
Andryerics <137793417+Andryerics@users.noreply.github.com>
2024-11-10 14:45:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
+# setup.py
+
+
 from setuptools import setup, find_packages
 
 setup(
-    name="gsearch",  
-    version="1.0.0",  
-    description="Search with Google",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    author="Andryerics",
-    author_email="andryerics@gmail.com",
-    url="https://github.com/andryerica1/Gsearch",  
-    license="MIT",
+    name='gsearch',
+    version='1.0.0',
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    install_requires=[
+        'requests',
     ],
-    python_requires=">=3.6",
+    include_package_data=True,
+    description='Search with Google',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Andryerics/gsearch',
+    author='Andry RL',
+    author_email='andryerics@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
